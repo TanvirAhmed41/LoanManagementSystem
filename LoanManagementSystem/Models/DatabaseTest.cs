10a3ec78bfdc5870cc74a5e73e0245da4e432b4b
Tanvir <santoahmed225@gmail.com>
2025-01-23 10:29:17 +0600

    first commit

---
+++ LoanManagementSystem/Models/DatabaseTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace LoanManagementSystem.Models
+{
+    public static class DatabaseTest
+    {
+        public static bool TestConnection()
+        {
+            string connectionString = @"Server=(LocalDb)\MSSQLLocalDB;Database=LoanManagementDB;Trusted_Connection=True;";
+
+            try
+            {
+                using (var connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    Console.WriteLine("Connection successful!");
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Connection failed: {ex.Message}");
+                return false;
+            }
+        }
+
+
+
+
+    }
+
+}
