f175084f0cd436ff5f94432aaac86edaeb12b164
Tanvir <santoahmed225@gmail.com>
2025-01-23 13:43:11 +0600

    showing memebrs in the table

---
+++ LoanManagementSystem/Controllers/MembersController.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Web.Mvc;
+using LoanManagementSystem.Models;
+
+namespace LoanManagementSystem.Controllers
+{
+    public class MembersController : Controller
+    {
+        private string connectionString = @"Server=(LocalDb)\MSSQLLocalDB;Database=LoanManagementDB;Trusted_Connection=True;";
+
+        // GET: Members
+        public ActionResult Index()
+        {
+            var members = GetMembers();
+            return View(members);
+        }
+
+        // GET: Members/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Members/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Member member)
+        {
+            if (ModelState.IsValid)
+            {
+                InsertMember(member);
+                return RedirectToAction("Index");
+            }
+            return View(member);
+        }
+
+        // Helper method to get members from the database
+        private List<Member> GetMembers()
+        {
+            var members = new List<Member>();
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                var command = new SqlCommand("SELECT * FROM Member", connection);
+                var reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    members.Add(new Member
+                    {
+                        MemberId = reader.GetInt32(0),
+                        Name = reader.GetString(1),
+                        Contact = reader.GetString(2),
+                        Email = reader.GetString(3),
+                        Address = reader.GetString(4),
+                        CreatedDate = reader.GetDateTime(5)
+                    });
+                }
+            }
+            return members;
+        }
+
+        // Helper method to insert a new member into the database
+        private void InsertMember(Member member)
+        {
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                var command = new SqlCommand("INSERT INTO Member (Name, Contact, Email, Address) VALUES (@Name, @Contact, @Email, @Address)", connection);
+                command.Parameters.AddWithValue("@Name", member.Name);
+                command.Parameters.AddWithValue("@Contact", member.Contact);
+                command.Parameters.AddWithValue("@Email", member.Email);
+                command.Parameters.AddWithValue("@Address", member.Address);
+                command.ExecuteNonQuery();
+            }
+        }
+
+    }
+}
---
+++ LoanManagementSystem/Models/Member.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace LoanManagementSystem.Models
+{
+    public class Member
+    {
+        internal DateTime CreatedDate;
+
+        public int MemberId { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [StringLength(50)]
+        public string Contact { get; set; }
+
+        [StringLength(100)]
+        public string Email { get; set; }
+
+        [StringLength(250)]
+        public string Address { get; set; }
+        public DateTime CreateDate { get; set; } = DateTime.Now;
+    }
+}
